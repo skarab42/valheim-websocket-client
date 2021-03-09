bec4de2f91070a6164e6076aec193175a7f4c2ff
skarab42 <skarab@onlfait.ch>
2021-03-09 12:23:39 +0100

    feat: Add logger module

--- src/index.js
+++ src/index.js
@@ -1,46 +1,13 @@
 const config = require("./config");
+const logr = require("./logr");
 
-const path = require("path");
-const execDir = path.dirname(process.execPath);
-require("dotenv-flow").config({ path: path.join(execDir, "../config") });
-
-const WebSocket = require("ws");
-
-const { ApiClient } = require("twitch");
-const { StaticAuthProvider } = require("twitch-auth");
-const { PubSubClient } = require("twitch-pubsub-client");
-
-const serverPort = process.env.VWS_SERVER_PORT;
-const clientId = process.env.TWITCH_CLIENT_ID;
-const accessToken = process.env.TWITCH_ACCESS_TOKEN;
-const huginRewardId = process.env.TWITCH_HUGIN_REWARD_ID;
-
-const authProvider = new StaticAuthProvider(clientId, accessToken);
-const apiClient = new ApiClient({ authProvider });
-const pubSubClient = new PubSubClient();
-
-let connected = false;
-
-const ws = new WebSocket(`ws://localhost:${serverPort}`, {
-  perMessageDeflate: false,
+logr.setSettings({
+  fileRotate: {
+    filename: config.app.logFile,
+    size: "5M",
+  },
 });
 
-ws.on("open", function open() {
-  console.log("connected");
-  connected = true;
-});
+const logger = logr("client");
 
-ws.on("close", () => {
-  console.log(">>> disconnected, exit...");
-  process.exit();
-});
-
-(async () => {
-  const userId = await pubSubClient.registerUserListener(apiClient);
-  await pubSubClient.onRedemption(userId, ({ rewardId, userName, message }) => {
-    if (connected && huginRewardId === rewardId) {
-      console.log(`>>> hugin/message/${userName}/${message}`);
-      ws.send(`hugin/message/${userName}/${message}`);
-    }
-  });
-})();
+logger.info(`${config.app.name} v${config.app.version}`);
---
+++ src/logr.js
@@ -0,0 +1,96 @@
+const chalk = require("chalk");
+const deepmerge = require("deepmerge");
+const colors = new chalk.Instance({ level: 3 });
+const fileStream = require("file-stream-rotator");
+
+let globalSettings = {
+  group: "logr",
+  level: "debug",
+  color: true,
+  fileRotate: {
+    filename: "logs/%DATE%.log",
+    date_format: "YYYY-MM-DD",
+    frequency: "custom",
+    verbose: false,
+    size: "5M",
+  },
+  levels: {
+    log: 0,
+    debug: 1,
+    info: 2,
+    warn: 3,
+    error: 4,
+  },
+  colors: {
+    debug: colors.white,
+    info: colors.blue,
+    warn: colors.yellow,
+    error: colors.red,
+  },
+};
+
+function writeLine(stream, line, ...data) {
+  if (data.length) {
+    line += ` ${JSON.stringify(data[0])}`;
+  }
+
+  stream.write(`[${new Date().toISOString()}]${line}\n`);
+}
+
+function log(settings, level, message, ...data) {
+  let logLevel = settings.levels[level] || levels.log;
+  let setLevel = settings.levels[settings.level] || levels.log;
+
+  if (logLevel < setLevel) {
+    return;
+  }
+
+  let color = settings.color ? settings.colors[level] : (t) => t;
+  let prefix = `[${settings.group}][${level}]`;
+
+  console[level](`${color(prefix)} ${message}`, ...data);
+
+  if (settings.stream) {
+    writeLine(settings.stream, `${prefix} ${message}`, ...data);
+  }
+}
+
+function logr(options = {}) {
+  if (typeof options === "string") {
+    options = { group: options };
+  }
+
+  const settings = deepmerge.all([
+    {},
+    globalSettings,
+    options,
+    { stream: null },
+  ]);
+
+  settings.stream = settings.fileRotate
+    ? fileStream.getStream(settings.fileRotate)
+    : null;
+
+  return {
+    log(level, message, ...data) {
+      log(settings, level, message, ...data);
+    },
+    debug(message, ...data) {
+      this.log("debug", message, ...data);
+    },
+    info(message, ...data) {
+      this.log("info", message, ...data);
+    },
+    warn(message, ...data) {
+      this.log("warn", message, ...data);
+    },
+    error(message, ...data) {
+      this.log("error", message, ...data);
+    },
+  };
+}
+
+logr.setSettings = (settings) =>
+  (globalSettings = deepmerge(globalSettings, settings));
+
+module.exports = logr;
